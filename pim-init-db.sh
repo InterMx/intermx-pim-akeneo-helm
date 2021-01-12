64db724d869326e8ff327eb3f6983114e853c9d5
Jayagopal Narayanaswamy <gopal@intermx.com>
2021-01-13 00:27:34 +0530

    IMXCLOUD-360 | Adding deployment

---
+++ pim-init-db.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+set -e
+
+# Wait for MySQL to be ready
+/usr/local/bin/wait-for-it.sh mysql-service:3306 -t 60
+
+# Wait for Elasticsearch to be ready
+/usr/local/bin/wait-for-it.sh elasticsearch-service:9200 -t 60
+
+# Init Akeneo database
+php -dmemory_limit=-1 /app/bin/console pim:installer:db --env=prod
